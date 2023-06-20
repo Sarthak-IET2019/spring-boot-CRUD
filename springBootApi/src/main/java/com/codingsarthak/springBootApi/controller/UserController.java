c09efc19aa7dad06a3db7d7e2131afd3f713ccb5
Sarthak <sarthakiet2019@gmail.com>
2023-06-20 21:42:49 +0530

    commit after user setup

--- springBootApi/src/main/java/com/codingsarthak/springBootApi/controller/UserController.java
+++ springBootApi/src/main/java/com/codingsarthak/springBootApi/controller/UserController.java
@@ -3,8 +3,10 @@ package com.codingsarthak.springBootApi.controller;
 import com.codingsarthak.springBootApi.model.Users;
 import com.codingsarthak.springBootApi.repositories.EmployeeRepository;
 import com.codingsarthak.springBootApi.repositories.UserRepository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,17 +18,36 @@ public class UserController {
 
 
     @PostMapping(value = "/signin")
-    private Users addEmployee(@RequestBody Users users){
+    private String UserSignIn(@RequestBody Users users){
 
 //        long roll_no= users.getRoll_No();
-//        if(userRepository.existsById(roll_no))
-//            return Exception;
-//            UserAlreadyExistException = new UserAlreadyExistException(roll_no)
-//            return u ;
-//
-//        }
-//        else
 
-        return userRepository.save(users);
+        if(!userRepository.existsById(users.getRoll_No())){
+            userRepository.save(users);
+            return users.getFirstName() + "SAVED";
+        }
+        else {
+            System.out.println("Roll No already exist, Please Login to Continue");
+            return "Customer Id already Exist";
+        }
+    }
+
+    @GetMapping (value = "/login")
+    private String UserLogin(@RequestBody Users users){
+
+        if(userRepository.existsById(users.getRoll_No())){
+            return users.getFirstName() + " LOGIN SUCCESS";
+        }
+
+        else {
+            System.out.println("No Roll No exist, Please Signin to Continue");
+            return "Please Sign In";
+        }
+    }
+
+
+    @DeleteMapping(value = "/login /{id}")
+    private void deleteEmployee(@PathVariable Long id){
+        userRepository.deleteById(id);
     }
 }
